6a22ea58c820a2773a8fa433d7badefebfe868d3
chizhan9527 <1346885811@qq.com>
2022-10-12 13:23:52 +0800

    update

--- src/main/java/com/chatroom/chatroomdemo/ChatroomDemoApplication.java
+++ src/main/java/com/chatroom/chatroomdemo/ChatroomDemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ChatroomDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChatroomDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ChatroomDemoApplication.class, args);
+    }
 
 }
