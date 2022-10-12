0602c9d24bffc221b4e8b185093759d15252a229
chizhan9527 <1346885811@qq.com>
2022-10-12 13:22:46 +0800

    Initial commit

---
+++ src/main/java/com/chatroom/chatroomdemo/ChatroomDemoApplication.java
@@ -0,0 +1,13 @@
+package com.chatroom.chatroomdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatroomDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatroomDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chatroom/chatroomdemo/ChatroomDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatroom.chatroomdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatroomDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
